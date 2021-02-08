1dffcc212b1d8d75a5f0d506f6366ac642316392
DavidHall-Code <david_m_hall@outlook.com>
2021-02-08 15:05:54 -0500

    Got initial compile to work

---
+++ src/snackBarApp/Main.java
@@ -0,0 +1,14 @@
+package snackBarApp;
+
+public class Main
+{
+  private static void workWithData()
+  {
+    System.out.println("Initial compile is working");
+  }
+
+  public static void main(String[] args)
+  {
+    workWithData();
+  }
+}
