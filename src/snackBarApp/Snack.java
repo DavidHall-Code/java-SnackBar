99a605e7664f81b90b06ee5800aab08ac11b2438
DavidHall-Code <david_m_hall@outlook.com>
2021-02-08 16:08:06 -0500

    Snack file is complete

--- src/snackBarApp/Snack.java
+++ src/snackBarApp/Snack.java
@@ -2,5 +2,83 @@ package snackBarApp;
 
 public class Snack
 {
-  
+  private static maxId = 0;
+
+  private int id;
+  private String name;
+  private int quantity;
+  private double cost;
+  private int vendingMachineId;
+
+// Constructor
+  public Snack(String name, int quantity, double cost, int vendingMachineId)
+  {
+    maxId++;
+    id = maxId;
+
+    this.name = name;
+    this.quantity = quantity;
+    this.cost = cost;
+    this.vendingMachineId =  vendingMachineId;
+  }
+
+// Getters and Setters
+// id
+  public int getId()
+  {
+    return id;
+  }
+
+// name
+  public String getName()
+  {
+    return name;
+  }
+  public void setName(String name)
+  {
+    this.name = name;
+  }
+
+// quantity
+  public int getQuantity()
+  {
+    return quantity;
+  }
+  public void setQuantity(int quantity)
+  {
+    this.quantity += quantity;
+  }
+
+// Cost
+  public double getCost()
+  {
+    return cost;
+  }
+  public void setCost(double cost)
+  {
+    this.cost = cost;
+  }
+
+// vendingMachineId
+  public int getVendingMachineId()
+  {
+    return vendingMachineId
+  }
+  public void setVendingMachineId(int vendingMachineId)
+  {
+    this.vendingMachineId = vendingMachineId;
+  }
+
+// buySnack
+  public void buySnackQuantity(int numToBuy)
+  {
+    this.quantity -= numToBuy;
+  }
+
+// total
+  public double getTotal(int quantity)
+  {
+    return this.cost * quantity;
+  }
+
 }
